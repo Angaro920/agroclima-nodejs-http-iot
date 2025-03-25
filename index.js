00867a48374e008c2224ebf0df55f26243ec8e27
Angaro <angaro1322@gmail.com>
2025-03-25 11:46:05 -0500

    Add meteorologic station variables

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ import axios from 'axios';
 
 
 const corsOptions = {
-  origin: "http://localhost:5173", 
+  origin: "http://localhost:5173",
   credentials: true,
 };
 
@@ -28,28 +28,33 @@ const PORT = 8000;
 
 const mongoURL = "mongodb://127.0.0.1:27017/AgroclimaAi";
 const dbName = "AgroclimaAi";
-const collectionTemp = "Temperatura";
-const collectionHum = "Humedad";
-const collectionHid = "Hidrogeno";
-const collectionLu = "Luz";
-const collectionTempStationIn = "temperaturaInterna";
-const collectionHumStationIn = "humedadInterna";
-
+const collectionTemp = "TemperaturaSensor";
+const collectionHum = "HumedadSensor";
+const collectionHid = "HidrogenoSensor";
+const collectionLu = "LuzSensor";
+const collectionTempStationIn = "TemperaturaInterna";
+const collectionHumStationIn = "HumedadInterna";
+const collectionTempStationOut = "TemperaturaExterna";
+const collectionHumStationOut = "HumedadExterna";
+const collectionUvStation = "Uv";
+const collectionRainStation = "Precipitaciones"
+const collectionRadiationStation = "RadiacionSolar"
+const collectionBarometricStation = "PresionBarometricaRelativa";
 
 
 mongoose.connect(mongoURL).then(() => {
-    console.log("Conectado a la base de datos")
-    app.listen(PORT, () => {
-        console.log(`Servidor HTTP corriendo en el puerto : ${PORT}`);
-    });
+  console.log("Conectado a la base de datos")
+  app.listen(PORT, () => {
+    console.log(`Servidor HTTP corriendo en el puerto : ${PORT}`);
+  });
 }).catch((error) => console.log(error));
 
 
 app.use("/api", routes);
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("<h1>Servidor WebSocket para DHT22</h1>");
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end("<h1>Servidor WebSocket para DHT22</h1>");
 });
 
 /* function horaActual (){
@@ -70,6 +75,13 @@ MongoClient.connect(mongoURL)
     const collectionL = db.collection(collectionLu);
     const collectionTStationIn = db.collection(collectionTempStationIn);
     const collectionHStationIn = db.collection(collectionHumStationIn);
+    const collectionTStationOut = db.collection(collectionTempStationOut);
+    const collectionHStationOut = db.collection(collectionHumStationOut);
+    const collectionUVstation = db.collection(collectionUvStation);
+    const collectionRStation = db.collection(collectionRainStation);
+    const collectionRadStation = db.collection(collectionRadiationStation);
+    const collectionPBStation = db.collection(collectionBarometricStation);
+
 
 
     wss.on("connection", (socket) => {
@@ -81,14 +93,14 @@ MongoClient.connect(mongoURL)
 
         try {
           const data = JSON.parse(messageStr);
-      
+
           const operations = [
             collectionT.insertOne({ data: data.temperatura, time: new Date() }),
             collectionHu.insertOne({ data: data.humedad, time: new Date() }),
             collectionHi.insertOne({ data: data.hidrogeno, time: new Date() }),
             collectionL.insertOne({ data: data.luz, time: new Date() }),
           ];
-      
+
           Promise.all(operations)
             .then(() => {
               console.log("Datos guardados correctamente en MongoDB");
@@ -96,7 +108,7 @@ MongoClient.connect(mongoURL)
             .catch((error) => {
               console.error("Error al guardar datos en MongoDB: ", error);
             });
-      
+
           // Send to all connected clients
           wss.clients.forEach((client) => {
             if (client.readyState === socket.OPEN) {
@@ -122,62 +134,93 @@ MongoClient.connect(mongoURL)
             applicationKey: process.env.AMBIENT_APP_KEY
           }
         });
-    
+
         const ambientData = response.data[0]?.lastData;
 
         if (ambientData?.tempinf !== undefined) {
           ambientData.tempinfC = ((ambientData.tempinf - 32) * 5 / 9).toFixed(2); // Keep 2 decimals
         }
-        
-    
+
+
         if (ambientData) {
-          const payload = JSON.stringify({ 
-            type: 'ambient', 
-            data:{ 
-              ...ambientData, 
-              tempinf: parseFloat(ambientData.tempinfC) 
+          const payload = JSON.stringify({
+            type: 'ambient',
+            data: {
+              ...ambientData,
+              tempinf: parseFloat(ambientData.tempinfC)
             }
           });
-    
+
           // Send to all connected clients
           wss.clients.forEach((client) => {
             if (client.readyState === WebSocket.OPEN) {
               client.send(payload);
             }
           });
-    
+
           console.log("Ambient data broadcasted:", ambientData);
 
           const saveOperations = [];
 
-          if(ambientData.tempinfC !== undefined) {
+          if (ambientData.tempinfC !== undefined) {
             saveOperations.push(
               collectionTStationIn.insertOne({ data: ambientData.tempinfC, time: new Date() })
             );
-        }
-        if (ambientData.humidityin !== undefined) {
-          saveOperations.push(
-            collectionHStationIn.insertOne({ data: ambientData.humidityin, time: new Date() })
-          );
-      }
+          }
+          if (ambientData.humidityin !== undefined) {
+            saveOperations.push(
+              collectionHStationIn.insertOne({ data: ambientData.humidityin, time: new Date() })
+            );
+          }
+          if (ambientData.tempf !== undefined) {
+            saveOperations.push(
+              collectionTStationOut.insertOne({ data: ambientData.tempf, time: new Date() })
+            );
+          }
+          if (ambientData.humidity !== undefined) {
+            saveOperations.push(
+              collectionHStationOut.insertOne({ data: ambientData.humidity, time: new Date() })
+            );
+          }
+          if (ambientData.uv !== undefined) {
+            saveOperations.push(
+              collectionUVstation.insertOne({ data: ambientData.uv, time: new Date() })
+            );
+          }
+          if (ambientData.solarradiation !== undefined) {
+            saveOperations.push(
+              collectionRadStation.insertOne({ data: ambientData.solarradiation, time: new Date() })
+            );
+          }
+          if (ambientData.eventrainin !== undefined) {
+            saveOperations.push(
+              collectionRStation.insertOne({ data: ambientData.eventrainin, time: new Date() })
+            );
+          }
+          if (ambientData.baromrelin !== undefined) {
+            saveOperations.push(
+              collectionPBStation.insertOne({ data: ambientData.baromrelin, time: new Date() })
+            );
+          }
 
-      Promise.all(saveOperations)
-      .then(() => {
-        console.log("Ambient internal data saved to MongoDB");
-      })
-      .catch((error) => {
-        console.error("Error saving ambient internal data:", error);
-      });
-    }
+
+          Promise.all(saveOperations)
+            .then(() => {
+              console.log("Ambient internal data saved to MongoDB");
+            })
+            .catch((error) => {
+              console.error("Error saving ambient internal data:", error);
+            });
+        }
 
       } catch (error) {
         console.error("Error fetching Ambient Weather data:", error.message);
       }
     };
-    
+
     // Fetch every 60 seconds
     setInterval(fetchAndBroadcastAmbientData, 60000);
-    
+
 
     server.listen(8080, () => {
       console.log("Servidor WebSocket corriendo en ws://localhost:8080");
